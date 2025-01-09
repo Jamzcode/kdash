ca62dfe73a4346f10dbd0a731fb929395cfc6c53
Jamzcode <129902520+Jamzcode@users.noreply.github.com>
2025-01-09 17:52:34 -0600

    added 5hr forecast

--- src/components/WeatherComponent.vue
+++ src/components/WeatherComponent.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="weather-widget">
     <button class="fetch-data" @click="getWeatherFromApi">FETCH WEATHER</button>
-    <h2 class="location">{{ location }}</h2>
-    <h1 class="current-temp">{{ currentTemp }}</h1>
-    <h3 class="current-condition">{{ currentCondition }}</h3>
-
     <button @click="getDateTime">GET DATE</button>
+    <button @click="getFiveHourForecast">GET 5 HOUR FORECAST</button>
 
+    <div class="current-condition">
+      <h1>Current Condition</h1>
+      <h2 class="location data">{{ location }}</h2>
+      <h2 class="temp data">{{ currentTemp }}</h2>
+      <h3 class="condition data">{{ currentCondition }}</h3>
+    </div>
     <section>
       <!-- TODO: Get Current Time -->
-      <h2 class="date">Date: {{ day }}</h2>
+      <div class="date">
+        <h2>Date:</h2>
+        <h3 class="data">{{ day }}</h3>
+      </div>
+
       <!-- TODO: Get Forecast for next two days -->
       <!-- convert JSON object into array to iterate for day and High/Low for following days -->
-      <h2 class="time">Time: {{ time }}</h2>
 
-      <div class="three-day-forecast">Three Day Forecast:</div>
-      <div class="hourly">40 | 40 | 40</div>
+      <div class="time">
+        <h2>Time:</h2>
+        <h3 class="data">{{ time }}</h3>
+      </div>
+
+      <h2 class="5-hour-forecast">5-hour forecast:</h2>
+
+      <div class="hourly data">{{ forecastTemp }}</div>
     </section>
   </div>
 </template>
@@ -32,6 +44,7 @@ export default {
       location: "",
       day: "",
       time: "",
+      forecastTemp: [],
     };
   },
   methods: {
@@ -61,6 +74,25 @@ export default {
         minute: "2-digit",
       });
     },
+    async getFiveHourForecast() {
+      const response = await axios.get(
+        "http://api.weatherapi.com/v1/forecast.json?key=de51034ece89422183d05652250801&q=Austin&days=5&aqi=no&alerts=yes"
+      );
+
+      let test = response.data;
+      let forecastDay = test.forecast.forecastday;
+
+      for (let i = 0; i < 6; i++) {
+        const averageTemp = forecastDay[i].day.avgtemp_f;
+        const result = Math.floor(averageTemp);
+        this.forecastTemp.push(result);
+      }
+      //Call Api to retrieve data for current day
+
+      //Create loop to iterate through hour[] objects
+
+      //Focus on 'temp_f' for each of 5 hour array
+    },
   },
 };
 </script>
@@ -68,5 +100,13 @@ export default {
 <style scoped>
 .weather-widget {
   border: solid gray 2px;
+}
+
+.data {
+  color: red;
+}
+
+.temp {
+  font-size: 50px;
 }
 </style>
